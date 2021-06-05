55b5d1a6c2a25b434664f2742fdeabbd8ca1bccb
xenomech <gokulsmenon227@gmail.com>
2021-06-05 12:49:40 +0530

    Migrating config

--- config.h
+++ config.h
@@ -4,18 +4,17 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
-};
-static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+	"Source Code Pro:size=15", "JoyPixels:pixelsize=15:antialias=true:autohint=true"};
+static const char *prompt = " ~> "; /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
 	[SchemeSel] = { "#eeeeee", "#005577" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
-/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
-
+/* -l and -g options; controls number of lines and columns in grid if > 0 */
+static unsigned int lines = 2;
+static unsigned int columns = 2;
 /*
  * Characters not considered part of a word while deleting words
  * for example: " /?\"&[]"
